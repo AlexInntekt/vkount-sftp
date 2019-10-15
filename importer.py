bc5412d40dbc7159c963cffda72c963a49239719
alexinntekt <alex.inntekt@gmail.com>
2019-10-15 14:11:24 +0300

    importer.py extracts data from csv and sends it to vkount with api

--- importer.py
+++ importer.py
@@ -1,6 +1,23 @@
 import csv
+import requests
+import json
+
+url = "https://cloud.v-count.com/api/v4/sales"
 
 with open('data_pack.csv', newline='') as File:  
     reader = csv.DictReader(File)
     for row in reader:
-        print(row['store'])
+        #print(row['sales'])
+
+        
+        payload = {'username':'ROMDemo',
+        'password':'ROMDemo1007',
+        'date':row['date'],
+        'sales': row['sales'],
+        'store':'RAV_DEMO',
+        'transaction':row['transaction'],
+        'checkouts':'1',
+        'units':'33'
+        }
+
+        response = requests.post(url, data=payload)
