8476124c2872e253da3a31e4fb375c546bec26c2
alexinntekt <alex.inntekt@gmail.com>
2019-10-18 14:55:56 +0300

    sp

---
+++ gather_traffic_data.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import csv
+import sys
+
+url = "https://cloud.v-count.com/api/v4/vcountapi"
+payload = {'username':'ROMDemo',
+		'password':'ROMDemo1007',
+		'format':'json',
+		'start_date':'2019-10-02',
+		'finish_date':'2019-10-15',
+		'store':'RAV_DEMO'}
+
+response = requests.post(url, data=payload)
+
+json_resp = response.json()
+
+
+
+
+outputFile = open("mypws.csv", 'w')
+output = csv.writer(outputFile)
+output.writerow(json_resp[0].keys())  # header row
+
+for row in json_resp:
+    output.writerow(row.values())
+
+
+# print(json.dumps(json_resp, sort_keys=True, indent=4))
+# print(response.text)
