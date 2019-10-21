8eee660d90476109c26d2e9c63055b51402460c8
alexinntekt <alex.inntekt@gmail.com>
2019-10-21 20:53:13 +0300

    sp

--- gather_traffic_data.py
+++ gather_traffic_data.py
@@ -2,29 +2,40 @@ import requests
 import json
 import csv
 import sys
-
-url = "https://cloud.v-count.com/api/v4/vcountapi"
-payload = {'username':'ROMDemo',
-		'password':'ROMDemo1007',
-		'format':'json',
-		'start_date':'2019-10-02',
-		'finish_date':'2019-10-15',
-		'store':'RAV_DEMO'}
-
-response = requests.post(url, data=payload)
-
-json_resp = response.json()
+from datetime import date
 
 
 
-
-outputFile = open("mypws.csv", 'w')
-output = csv.writer(outputFile)
-output.writerow(json_resp[0].keys())  # header row
-
-for row in json_resp:
-    output.writerow(row.values())
+def return_current_day():
+	today = date.today()
+	print(today)
 
 
-# print(json.dumps(json_resp, sort_keys=True, indent=4))
-# print(response.text)
+def run():
+	url = "https://cloud.v-count.com/api/v4/vcountapi"
+	payload = {'username':'ROMDemo',
+			'password':'ROMDemo1007',
+			'format':'json',
+			'start_date':'2019-10-02',
+			'finish_date':'2019-10-15',
+			'store':'RAV_DEMO'}
+
+	response = requests.post(url, data=payload)
+
+	json_resp = response.json()
+
+
+
+
+	outputFile = open("traffic_data_showroom.csv", 'w')
+	output = csv.writer(outputFile)
+	output.writerow(json_resp[0].keys())  # header row
+
+	for row in json_resp:
+	    output.writerow(row.values())
+
+
+	# print(json.dumps(json_resp, sort_keys=True, indent=4))
+	# print(response.text)
+
+return_current_day()
